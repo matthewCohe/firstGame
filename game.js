b9ae061f68350e6a80beb78d0451eccde5bee7c6
matthewCohe <75818291+matthewCohe@users.noreply.github.com>
2021-01-18 21:20:29 -0500

    Se incializa el repositorio reomoto rn github
    Se subieron los avances del primer juego para tener un respaldo y tener un versionado de los avances

---
+++ game.js
@@ -0,0 +1,61 @@
+var canvas = null,
+    ctx = null;
+    var x = 50,
+    y = 50;
+
+    var KEY_LEFT = 37, 
+        KEY_UP = 38,
+        KEY_RIGHT = 39,
+        KEY_DOWN = 40;
+
+        var dir = 0;
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    run();
+    repaint();
+}
+    
+function paint(ctx) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
+
+    ctx.fillText('Last Press: ' + lastPress, 0, 20);
+    
+}
+
+function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
+}
+
+function act(){
+    x += 2;
+    if (x > canvas.width){
+        x=0
+    }
+}
+
+function repaint() {
+    window.requestAnimationFrame(repaint);
+    paint(ctx);
+}
+
+function run() {
+    setTimeout(run, 50);
+    act();
+}
+var lastPress = null;
+
+
+document.addEventListener('keydown', function (evt) {
+    lastPress = evt.which;
+}, false);
+
+window.addEventListener('load', init, false);
+
